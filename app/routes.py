95c50f1cd44203a5683200e34a2ae91de5d00f2f
AsiaSaja <lukmanpakiki@gmail.com>
2024-11-24 14:25:08 +0700

    Pesan commit

--- app/routes.py
+++ app/routes.py
@@ -21,3 +21,26 @@ def add_item():
         return redirect(url_for('index'))  # Redirect ke halaman index
 
     return render_template('add_item.html')
+
+@app.route('/delete/<int:item_id>', methods=['POST'])
+def del_item(item_id):
+    item = Item.query.get(item_id)  # Menggunakan item_id yang diterima sebagai parameter
+    if item:
+        # Hapus item dari database
+        db.session.delete(item)
+        db.session.commit()
+        return redirect(url_for('index'))  # Redirect ke halaman utama
+    else:
+        return "Item not found", 404
+
+@app.route('/edit/<int:item_id>', methods=['GET', 'POST'])
+def edit_item(item_id):
+    item = Item.query.get_or_404(item_id)  # Ambil item berdasarkan ID atau tampilkan 404 jika tidak ditemukan
+    if request.method == 'POST':
+        # Update item berdasarkan data dari form
+        item.name = request.form['name']
+        item.description = request.form['description']
+        db.session.commit()  # Simpan perubahan ke database
+        return redirect(url_for('index'))  # Redirect ke halaman index
+
+    return render_template('edit_item.html', item=item)
